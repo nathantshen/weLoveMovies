854c38e35ad4b2ef20a9f5ad1eb7c50dbd7e85a3
Nathan T Shen <114704136+nathantshen@users.noreply.github.com>
2023-03-29 15:32:51 -0700

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -26,7 +26,7 @@ module.exports = {
 
   production: {
     client: "postgresql",
-    connection: process.env.DATABASE_URL,
+    connection: 'postgres://ycumbxcr:BPgGLZEbnEQ382q7EF1rsTzFFLhn91Bi@floppy.db.elephantsql.com/ycumbxcr',
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
